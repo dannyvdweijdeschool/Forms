3aa4abd0aef045dd1b1fdad47003d46c7cc4cdda
Unknown <99053034@mydavinci.nl>
2019-02-19 14:01:40 +0100

    Form
    Ik heb de lab opdrachten 1 t/m 3 gemaakt.

---
+++ Lab 2/php/welcome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+	<title>php</title>
+</head>
+<body>
+	<h1>De ingevulde gegevens zijn:</h1></br>
+	<p><?php echo $_POST["name"]; ?></p>
+	<p><?php echo $_POST["e_mail"]; ?></p>
+</body>
+</html>
+
+<!-- Vragen opdracht 1
+1: De antwoorden staan er in.
+2: Als je belangrijke informatie gebruikt zoals wachtwoorden.
+3: Als het voor de gebruiker wel handig is om te zien wat hij/zij heeft ingevuld. -->
+
+<!-- Vragen opdracht 2 
+1: Als het belangrijke informatie is zoals wachtwoorden.
+2: Niet echt.
+1-->
---
+++ Lab 3/PHP/form.php
@@ -0,0 +1,83 @@
+<?php 
+	$name = "";
+	$email = "";
+	$nameErr = "";
+	$form = false;
+	$errors = 0;
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+	  $name = test_input($_POST["name"]);
+	  $email = test_input($_POST["e_mail"]);
+	}
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+		if (empty($_POST["name"])){
+		    $nameErr = "Name is nodig";
+		    $errors++;
+		}elseif(!preg_match("/^[a-zA-Z ]*$/",$name)){
+		    $nameErr = "Je mag alleen letters en witruimte gebruiken.";
+		    $errors++;
+		}else{
+			$name = test_input($_POST["name"]);
+		}
+	}
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+		if (empty($_POST["e_mail"])){
+			$emailErr = "E-mail is nodig";
+			$errors++;
+		}elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+	        $emailErr = "ongeldig e-mail address."; 
+	        $errors++;
+	    }else{
+		    $email = test_input($_POST["e_mail"]);
+		}
+
+	if($errors == 0){
+		$form = true;
+	}
+}
+
+function test_input($data) {
+  $data = trim($data); //Zorgt ervoor dat onnodige space, tab, newline worden weggehaald.
+  $data = stripslashes($data); //verwijderd backslashes (\).
+  $data = htmlspecialchars($data); //Dit zorgt ervoor dat speciale karakters naar html veranderd waardoor je niet gehackt kan worden.
+  return $data;
+}
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+	<title>form</title>
+	<link rel="stylesheet" type="text/css" href="../CSS/style.css">
+</head>
+<body>
+<?php if($form == false){ ?>
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<fieldset>
+			<legend>Gegevens:</legend>
+			<div>
+				<label>Name:</label>
+				<input type="text" name="name" value="<?php echo $name;?>">
+				<span class="error">* <?php echo $nameErr;?></span>
+			</div>
+			<div>
+				<label>E-mail:</label>
+				<input type="text" name="e_mail" value="<?php echo $email;?>">
+				<span class="error">* <?php echo $emailErr;?></span>
+			</div>
+			<div id="submit">
+				<input type="submit" value="Submit">
+			</div>
+		</fieldset>
+	</form>
+<?php } else{?>
+	<p><?php echo $name ?></p>
+	<p><?php echo $email ?></p>
+<?php } ?>
+</body>
+</html>
+
+
+<!-- Antwoord: Hij voert de alert uit. -->
